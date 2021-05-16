2bc9bc23f0a4f5bd958086ac046e5b743a15202f
Johnny Kung <kjohnathan.engi@gmail.com>
2021-05-15 22:26:36 -0700

    fix url in socket

--- src/util/socketIO.util.js
+++ src/util/socketIO.util.js
@@ -1,6 +1,6 @@
 import socketIO from "socket.io-client";
 
-const socket = socketIO("http://localhost:8000", { 
+const socket = socketIO("http://idsp.link", { 
     withCredentials: true,
     autoConnect: false
 });
